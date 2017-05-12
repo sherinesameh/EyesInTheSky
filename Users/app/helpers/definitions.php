897dddbed1a2b79c269f15dbc111b53148d74c6c
sherinesameh <sherine-sameh@hotmail.com>
2017-05-12 21:56:03 +0200

    make the master branch for server stuff only

--- Users/app/helpers/definitions.php
+++
@@ -1,7 +0,0 @@
-<?php
-  define('LOGGED_IN_SUCCESSFULY', true);
-  define('LOGGED_IN_ERROR', false);
-  define('CREATED_SUCCESSFULY', true);
-  define('ALREADY_EXIST', false);
-  define('ERROR', false);
-?>
