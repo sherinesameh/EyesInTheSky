7c6c6e3e9d0bc5f49d45fa32f2040ab445d28635
yamen emad <yamenemad4@gmail.com>
2017-07-06 05:23:08 +0200

    Add files via upload

--- app/controllers/upload.php
+++ app/controllers/upload.php
@@ -1,11 +1,13 @@
 <?php
 require("socket.php");
 require("../models/user.php");
-// $socket = new Socket;
 $user = new User;
+$socket = new Socket;
+session_start();
+$id = $_SESSION['Id'];
 
-$processName = $_POST['processName'];
-echo $_FILES['dockerfile']['name'];
+$processName =$_POST['processName'];
+
 if (isset($_FILES['dockerfile'])) {
 	$name_array = $_FILES['dockerfile']['name'];
 	$temp_array = $_FILES['dockerfile']['tmp_name'];
@@ -13,30 +15,38 @@ if (isset($_FILES['dockerfile'])) {
 	$size_array = $_FILES['dockerfile']['size'];
 	$error_array = $_FILES['dockerfile']['error'];
 
-	if(!is_dir("../../test_uploads/")) {
-		$oldmask = umask(0);
-	mkdir("../../test_uploads/", 0777);
-	umask($oldmask);
-	}
 	$check_docker = 0;
-		for ($i=0; $i < count($name_array); $i++) {
-			if (move_uploaded_file($temp_array[$i], "../../test_uploads/".$name_array[$i])) {
-				if (!strcmp($name_array[$i], "Dockerfile")) {
-					$check_docker = $check_docker + 1;
-				}
+		if (!strcmp($name_array, "Dockerfile")) {
+				$check_docker = $check_docker + 1;
 			}
-			chmod("../../test_uploads/".$name_array[$i], 0777);
+
+	if ($check_docker == 0) {
+				echo json_encode("1");
+		}  else {
+			##make directory 
+
+			$date = date("Y-m-d_h:i:s");
+            $filename = $date."_".$id."_".$processName;
+			$oldmask = umask(0);
+	        mkdir("../../uploads/".$filename."/", 0777);
+	        umask($oldmask);
+
+	        $directory = "../../uploads/".$filename."/";
+	        #put files in the directory   
+			
+				if (move_uploaded_file($temp_array, $directory.$name_array)) {
+					chmod($directory.$name_array, 0777);
+				}
+			
+
+		    #send to server
+			$user->addProcessInprogress($id,$processName);
+			$path= "/opt/lampp/htdocs/Users/uploads/".$filename;
+			$socket->send($path,$processName,$id);
+			echo json_encode("3");
+
 		}
-		if ($check_docker == 0) {
-				echo "no Dockerfile exist <br>";
-		} elseif ($check_docker > 1) {
-				echo "there is more than one Dockerfile <br>";
-		} else {
-				$name = "webServer";
-				$id  = 1;
-				$path= "/opt/lampp/htdocs/Users/test_uploads";
-				$socket->send($path);
-				$socket->receive();
-		}
+
+		
 	}
 ?>
