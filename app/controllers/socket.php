ae3bc1186f7235caff382f73820311dff3932fdc
sherinesameh <sherine-sameh@hotmail.com>
2017-07-07 22:31:58 +0200

    client side updates

--- app/controllers/socket.php
+++ app/controllers/socket.php
@@ -1,10 +1,10 @@
 <?php
 class Socket
 {
-	private $host = "46.101.180.169";
+	// private $host = "46.101.180.169";
+  private $host ="localhost";
   private $port = 8080;
   private $socket;
-
 	function __construct()
 	{
 		$this->socket = socket_create(AF_INET, SOCK_STREAM,0) or die("Could not create socket\n");
@@ -12,13 +12,11 @@ class Socket
     $type ="40307";
 		socket_write($this->socket, $type, strlen ($type)) or die("Could not write output\n");
 	}
-	function send($path)
+	function send($path,$processName,$userID)
 	{
-		$type ='40307';
-		$output = $path;
-		socket_write($this->socket, $type, strlen ($type)) or die("Could not write output\n");
-		sleep(1);
+		$output = $path.":_:".$processName.":_:".$userID;
 		socket_write($this->socket, $output, strlen ($output)) or die("Could not write output\n");
+		$this->close();
 	}
 	function receive()
 	{
@@ -30,4 +28,3 @@ class Socket
       socket_close($this->socket) ;
 	}
 }
-?>
