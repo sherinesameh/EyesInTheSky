2c3dbd31f1350657b6ad7e170c7c0dbeacedc72d
yamen emad <yamenemad4@gmail.com>
2017-07-09 20:18:32 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 import datetime
+from   dbHandler import dbHandler
 import os
 import socket
 import subprocess
 import sys
 import threading
-
-from   EITS.dbHandler import dbHandler
-from EITS import utilities
+import utilities
 
 HOST = ''
 PORT = 8080
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 NUMBEROFTHREADS = 10
 CONNECTIONS = {}
+DB = dbHandler()
 
 def setupConnection():
     try:
@@ -42,15 +42,15 @@ def acceptConnections():
         thread = threading.Thread(target = TYPES[type] , kwargs={'connection': connection})
         thread.daemon = True
         thread.start()
-        print("====================================================================================")
         print("Connections has been established | IP: " + address[0] + " | Port: " + str(address[1]))
-        print("====================================================================================")
 
 def connectRP(connection):
-    handshakePck = str(connection.recv(90))
+    handshakePck = str(connection.recv(80))
+    print("el handshak "+handshakePck)
     specs = handshakePck.split(':_:')
-    dbHandler.addPi(specs[0],specs[1],specs[2],specs[3],specs[4],specs[5])
+    DB.addPi(specs[0],specs[1],specs[2],specs[3],specs[4],specs[5])
     mac = specs[1]
+    print("El mac eli gayely "+mac)
     CONNECTIONS[mac] = connection
 
 def adminCommands(connection):
@@ -71,15 +71,15 @@ def adminCommands(connection):
        connectionRP = CONNECTIONS[mac]
        print("el mac eli ray7lo "+mac)
        connectionRP.send('shutdw')
-       dbHandler.shutPi(AdminID, mac )
+       DB.shutPi(AdminID, mac )
     if cmd[0] == "87452":
        mac = cmd[1]
        connectionRP = CONNECTIONS[mac]
        connectionRP.send('restrt')
-       dbHandler.restartPi(AdminID, mac )           
+       DB.restartPi(AdminID, mac )           
 
 def userCommands(connection):
-    mac = dbHandler.getBestPi()
+    mac = DB.getBestPi()
     print(mac)
     print("mac is "+mac)
 
@@ -96,8 +96,20 @@ def userCommands(connection):
         connectionRP.send('rundocker')
     results = str(connectionRP.recv(1024)).decode('utf-8')
     print("results iare "+results)
-    dbHandler.updateResults(userID , processName , results)
+    DB.updateResults(userID , processName , results)
 
+# def userCommands(connection):
+    # bestPi = dbHandler()
+    # mac = bestPi.getBestPi()
+    # hostname = CONNECTIONS[mac]
+    # specs = str(bestPi.getSpecs(mac)).split(':')
+    # username = specs[0]
+    # password = specs[1]
+    # localPath = str(connection.recv(1024)).decode('utf-8')
+    # remotePath = utilities.createPiDir()
+    # sftp = utilities.put(hostname, username, password, localPath, remotePath)
+    # if sftp:
+    #     connection.send(utilities.buildAndRunDocker(hostname, username, password, remotePath))
 
 def govCommands(connection):
 
@@ -117,7 +129,7 @@ def govCommands(connection):
     #specific to pi-s in specific locations    
         locations = str(connection.recv(1024)).strip().split(":_:")
         print(locations)
-        macs = dbHandler.getLocatedPis(locations)
+        macs = DB.getLocatedPis(locations)
         for x in macs:
             connectionRP = CONNECTIONS[x]
             sendToCamera(connectionRP)
