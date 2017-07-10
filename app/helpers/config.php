3591e9d15a7bbcdb7dd9fad8ce0b1eae3607b26a
sherinesameh <sherine-sameh@hotmail.com>
2017-07-10 13:40:00 +0200

    final edits

--- app/helpers/config.php
+++ app/helpers/config.php
@@ -1,6 +1,6 @@
 <?php
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', 'sherine');
-  define('DB_HOST', 'localhost');
+  define('DB_USERNAME', 'pi');
+  define('DB_PASSWORD', 'eits2017');
+  define('DB_HOST', '46.101.180.169');
   define('DB_NAME', 'EITS');
 ?>
