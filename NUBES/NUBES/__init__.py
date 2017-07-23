51eb399be91b7b1b7139666d2b91cf106b754cf7
sherif <mohamed--sherif@hotmail.com>
2017-07-23 18:24:01 +0100

    Final-Version

--- NUBES/NUBES/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-from __future__ import absolute_import
-
-from NUBES.openface.align_dlib import AlignDlib
-from NUBES.openface.torch_neural_net import TorchNeuralNet
-from NUBES.util.dbHandler import dbHandler
